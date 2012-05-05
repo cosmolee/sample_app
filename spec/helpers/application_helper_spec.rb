b1df4506cd149adcbf591610275ebff1007f8343
Cosmo Lee <cosmolee@cosmolee.com>
2012-05-05 10:50:35 -0400

    Finished Chap 5 exercizes

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+#Ex 5.6.3
+
+#Michael:
+
+#This exercize is confusing.  Not sure what is being accomplished here?  The code in listing 5.37 doesn't seem to be accomplishing anything.  Indeed, completely commenting out the code in 5.37 allows the test to run successfully!
+
+
+#describe ApplicationHelper do
+
+#  describe "full_title" do
+#    it "should include the page title" do
+#      full_title("foo").should =~ /foo/
+#    end
+
+#    it "should include the base title" do
+#      full_title("foo").should =~ /^Ruby on Rails Tutorial Sample App/
+#    end
+
+#    it "should not include a bar for the home page" do
+#      full_title("").should_not =~ /\|/
+#    end
+#  end
+#end
+
+
+
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  subject { page }           # Using the (Capybara) `subject` block allows condensed `it` blocks w/o the `page.*` naming.  See "Help" tests for UN-condensed version of tests.
+  subject { page }           
+  # Using the (Capybara) `subject` block allows condensed `it` blocks w/o the `page.*` reference.  See "Help page" tests for UN-condensed version of tests for comparison.
+  # Sect 5.3.4: Because of subject { page }, the call to should automatically uses the page variable supplied by Capybara .
+
+ shared_examples_for "all static pages" do  # This is for the blocks to use the "shared examples" facility of RSpec.
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }  # See spec utilities directory for `full_title()` helper.
+  end
+
 
   describe "Home page" do      # Sec 5.3.4: Condensed version of tests.
     before { visit root_path } # The `before` block runs before each `it` block.  Synonym: `before(:each)`
@@ -9,20 +17,6 @@ describe "Static pages" do
     it { should have_selector('h1', text: 'Sample App') }
     it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector('title', text: '| Home') }
-  end
-
-  describe "About page" do
-    before { visit about_path }
-
-    it { should have_selector('h1', text: 'About Us') }
-    it { should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us") }
-  end
-
-  describe "Contact page" do
-    before { visit contact_path }
-
-    it { should have_selector('h1', text: 'Contact') }
-    it { should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Contact") }
   end
 
 
@@ -42,5 +36,60 @@ describe "Static pages" do
     end
   end
 
+
+  describe "About page" do  # Ex 5.6.1, using "shared examples" facility, further condensing tests...
+    before { visit about_path }
+    let(:heading)    { 'About' }
+    let(:page_title) { 'About Us' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+    let(:heading)    { 'Contact' }
+    let(:page_title) { 'Contact' }
+
+    it_should_behave_like "all static pages"
+  end
+
+
+# These test make sure that the proper links exist by actually clicking on them, rather than telling the tests the path with `visit`.  
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    page.should_not have_selector 'title', text: full_title('Home')
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+  end
+
+
+
+
+
+#  describe "should have the right links on the layout" do  
+#    before { visit root_path }
+#    click_link "About"
+#    it { should have_selector('title', text: '| About') }
+#    page.should have_selector 'title', text: full_title('About Us')
+#    click_link "Help"
+#    it { should have_selector('title', text: '| Help') }
+#    page.should # fill in
+#    click_link "Contact"
+#    it { should have_selector('title', text: '| Contact') }
+#    page.should # fill in
+#    click_link "Home"
+#    it { should_not have_selector('title', text: '| Home') }
+#    page.should # fill in
+#    click_link "Sign up now!"
+#    it { should have_selector('title', text: '| Sign up') }
+#    page.should # fill in
+#  end
 
 end
