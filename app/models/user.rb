5d61a71a3f9a60a9cd8eb88a17d986afdcac8237
Cosmo Lee <cosmolee@cosmolee.com>
2012-05-15 16:16:25 -0400

    Finish Chap 7 Exercizes

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,19 +14,21 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }  # ROR Tutorial 7.6.3: `presence` attribute is removed to avoid duplicate error messages.
   validates :password_confirmation, presence: true
   # The API for has_secure_password states: "Validations for presence of password, confirmation of password (using a "password_confirmation" attribute) are automatically added."
-  # But the rspec tests fail without the explicit validation for :password_confirmation presence...
+  # But the rspec model test fails without the explicit validation for :password_confirmation presence...
 
 
   validates :name, presence: true, length: { maximum: 50 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, 
+                    format: { with: VALID_EMAIL_REGEX }, 
+                    uniqueness: { case_sensitive: false }
 
-  before_save { |user| user.email = email.downcase }
   # Per ROR Tutorial 2nd Ed, listing 6.23: "...not all database adapters use case-sensitive indices"
+  before_save { |user| user.email = email.downcase }
 
 
 
